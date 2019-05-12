63e997415d63ef9d42137d608d68fc16229afa35
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-12 13:37:24 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,23 +4,5 @@ function updatePlaylist (playlist, artistName, songTitle){
   playlist[artistName]=songTitle
 }
  function removeFromPlaylist(playlist,artistName){ 
-delete playlist[artistName];
-}
-function removeFromPlaylist(playlist, artistName){
-delete playlist[artistName];
-}
-
-var meals = {};
-
-var meals = new Object ();
-
-var meals = { breakfast: "oatmeal" };
- 
-// or, equivalently
- 
-var meals = new Object({ breakfast: 'oatmeal' })
-
-var meals = {
-  breakfast: 'eggs',
-  breakfast: 'bacon'
+delete playlist[artistName]
 }
